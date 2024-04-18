76e2c9a22acb1a41307db1582eb48bb80bdcf4da
Jonas Ross <jonasrossaa@gmail.com>
2024-04-18 16:00:59 -0600

    Stub out login / signup pages - Jonas

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,20 @@
+import Link from 'next/link';
+
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div>
-        <h1 className="text-4xl font-bold">Placeholder</h1>
+    <main className="flex min-h-screen flex-col md:flex-row">
+      <div className="bg-gray-700 p-4 md:w-1/3 md:p-24">
+        <div>Quickly Code Challenge</div>
+      </div>
+      <div className="flex items-center justify-center p-20 md:w-2/3 md:p-0">
+        <div>
+          <h1 className="text-4xl font-bold">Get started</h1>
+          <div>
+            <Link href="/signup">Create an account</Link>
+            <div>or</div>
+            <Link href="/login">Sign in</Link>
+          </div>
+        </div>
       </div>
     </main>
   );
---
+++ src/app/signup/page.tsx
@@ -0,0 +1,9 @@
+export default function Signup() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <div>
+        <h1 className="text-4xl font-bold">Signup</h1>
+      </div>
+    </main>
+  );
+}
