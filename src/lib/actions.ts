ae864a34e95c514d5d394c1103ad16b97190aa2d
Jonas Ross <jonasrossaa@gmail.com>
2024-04-18 21:19:20 -0600

    Fix validation and document oddities I ran into with zod  - Jonas

--- src/lib/actions.ts
+++ src/lib/actions.ts
@@ -4,104 +4,40 @@ import { z } from 'zod';
 import { revalidatePath } from 'next/cache';
 import { redirect } from 'next/navigation';
 
-const SignupSchema = z
-  .object({
-    user: z.object({
-      first_name: z.string(),
-      last_name: z.string(),
-      email: z.string().email({ message: 'Invalid email address.' }),
-      confirm_email: z.string().email({ message: 'Invalid email address.' }),
-      password: z.string().min(6),
-      confirm_password: z.string().min(6),
-    }),
-    company: z.object({
-      activity: z.object({
-        early_pay_intent: z.boolean(),
-        expected_activity: z.string(),
-      }),
-      early_pay_intent: z.boolean(),
-      industry: z.object({
-        value: z.string(),
-        label: z.string(),
-      }),
-      business_type: z.object({
-        value: z.string(),
-        label: z.string(),
-      }),
-      website: z.string().optional(),
-      business_registration: z.string(),
-      phone: z.string(),
-      business_number: z.string(),
-      has_trade_name: z.boolean(),
-      legal_name: z.string(),
-      expected_activity: z.string(),
-    }),
-  })
-  .superRefine(({ user, company }, ctx) => {
-    if (user.email !== user.confirm_email) {
-      ctx.addIssue({
-        code: z.ZodIssueCode.custom,
-        message: 'Emails do not match.',
-      });
-    }
-    if (user.password !== user.confirm_password) {
-      ctx.addIssue({
-        code: z.ZodIssueCode.custom,
-        message: 'Passwords do not match.',
-      });
-    }
-  });
-
-const UserSchema = z
-  .object({
+const SignupSchema = z.object({
+  user: z.object({
     first_name: z.string(),
     last_name: z.string(),
     email: z.string().email({ message: 'Invalid email address.' }),
     confirm_email: z.string().email({ message: 'Invalid email address.' }),
     password: z.string().min(6),
     confirm_password: z.string().min(6),
-  })
-  .superRefine(({ email, confirm_email, password, confirm_password }, ctx) => {
-    if (email !== confirm_email) {
-      ctx.addIssue({
-        code: z.ZodIssueCode.custom,
-        message: 'Emails do not match.',
-      });
-    }
-    if (password !== confirm_password) {
-      ctx.addIssue({
-        code: z.ZodIssueCode.custom,
-        message: 'Passwords do not match.',
-      });
-    }
-  });
-
-const CompanySchema = z.object({
-  activity: z.object({
-    early_pay_intent: z.boolean(),
+  }),
+  company: z.object({
+    activity: z.object({
+      early_pay_intent: z.boolean(),
+      expected_activity: z.string(),
+    }),
+    industry: z.object({
+      value: z.string(),
+      label: z.string(),
+    }),
+    business_type: z.object({
+      value: z.string(),
+      label: z.string(),
+    }),
+    website: z.string().optional(),
+    business_registration: z.string(),
+    phone: z.string(),
+    business_number: z.string(),
+    has_trade_name: z.boolean(),
+    legal_name: z.string(),
     expected_activity: z.string(),
   }),
-  early_pay_intent: z.boolean(),
-  industry: z.object({
-    value: z.string(),
-    label: z.string(),
-  }),
-  business_type: z.object({
-    value: z.string(),
-    label: z.string(),
-  }),
-  website: z.string().optional(),
-  business_registration: z.string(),
-  phone: z.string(),
-  business_number: z.string(),
-  has_trade_name: z.boolean(),
-  legal_name: z.string(),
-  expected_activity: z.string(),
 });
 
 export async function createUser(prevState: any, formData: FormData) {
-  console.log('Creating user...');
-  console.log(formData);
+  // For some reason checkboxes are not being serialized as 'on' when checked, so we need to check for that. Don't know why. Might research later.
   const validatedFields = SignupSchema.safeParse({
     user: {
       first_name: formData.get('first_name'),
@@ -113,7 +49,7 @@ export async function createUser(prevState: any, formData: FormData) {
     },
     company: {
       activity: {
-        early_pay_intent: formData.get('early_pay_intent'),
+        early_pay_intent: formData.get('early_pay_intent') === 'on',
         expected_activity: formData.get('expected_activity'),
       },
       early_pay_intent: formData.get('early_pay_intent'),
@@ -129,20 +65,39 @@ export async function createUser(prevState: any, formData: FormData) {
       business_registration: formData.get('business_registration'),
       phone: formData.get('phone'),
       business_number: formData.get('business_number'),
-      has_trade_name: formData.get('has_trade_name'),
+      has_trade_name: formData.get('has_trade_name') === 'on',
       legal_name: formData.get('legal_name'),
       expected_activity: formData.get('expected_activity'),
     },
   });
 
-  console.log(validatedFields);
-
   // If form validation fails, return errors early. Otherwise, continue.
   if (!validatedFields.success) {
-    console.log(validatedFields.error.issues);
     return {
       errors: validatedFields.error.flatten().fieldErrors,
       message: 'Missing Fields. Failed to Create Account.',
+    };
+  }
+
+  // Should be a way to do these 2 with zod but refinements are not working for me right now and I don't want to spend more time on dealing with it.
+  const emailsMatch = formData.get('email') === formData.get('confirm_email');
+  const passwordsMatch =
+    formData.get('password') === formData.get('confirm_password');
+  if (!emailsMatch || !passwordsMatch) {
+    return {
+      errors: {
+        user: {
+          ...(!emailsMatch && { email: ['Email addresses do not match.'] }),
+          ...(!emailsMatch && {
+            confirm_email: ['Email addresses do not match.'],
+          }),
+          ...(!passwordsMatch && { password: ['Passwords do not match.'] }),
+          ...(!passwordsMatch && {
+            confirm_password: ['Passwords do not match.'],
+          }),
+        },
+      },
+      message: 'Failed to Create Account.',
     };
   }
 
